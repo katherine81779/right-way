6ab1ac85ebc6b9625cb78ef096f556a69dc4bbc7
katherine81779 <katherine81779@gmail.com>
2020-01-18 23:58:23 -0800

    acquire information from APIS

--- getAPI.py
+++ getAPI.py
@@ -1,27 +1,44 @@
 import json
 import urllib.parse
-import urllib.request
+from urllib.request import Request, urlopen
+import requests
 
-BASE_URL = 'https://www.carqueryapi.com/api/0.3/?callback=?&cmd=getTrims&' # this is to get the model id
+BASE_URL = 'https://www.carqueryapi.com/api/0.3/?cmd=getTrims&' # this is to get the model id
 
-def getModelID():
-    brand = input()
-    year = input()
-    model = input()
-
+def getBasicURL(brand, model, year):
     query_parameters = [('make', brand), ('model', model), ('year', year)]
     return BASE_URL + urllib.parse.urlencode(query_parameters)
     
-def get_result(url):
-    response = None
-    try:
-        response = urllib.request.urlopen(url)
-        json_text = response.read().decode(encoding = 'utf-8')
-        return json.loads(json_text)
-    finally:
-        if response != None:
-            response.close
-def printDescription(searchInfo):
-    print(searchInfo['model_id'])
+def getBasicResult(url):
+    response = Request(url, headers = {'User-Agent': 'Mozilla/5.0'})
+    json_text = urlopen(response).read()
+    return json.loads(json_text)
 
-printDescription(get_result(getModelID()))
+def getCarDictBase(searchInfo):
+    carDict = searchInfo['Trims'][0]
+    return carDict
+
+baseURL = getBasicURL('Toyota', 'Camry', '2008')
+modelInfo = getCarDictBase(getBasicResult(baseURL))
+
+modelID = modelInfo['model_id']
+
+# Things that we need to print
+bodyType = modelInfo['model_body']
+seatsNum = modelInfo['model_seats']
+doorsNum = modelInfo['model_doors']
+origin = modelInfo['make_country']
+weight = modelInfo["model_weight_kg"] # Display: heavier cars makes it safer to drive
+length = modelInfo["model_length_mm"] 
+width = modelInfo["model_width_mm"]
+height = modelInfo["model_height_mm"] # Display: Taller people should get taller cars for comfortable driving
+
+family = False
+if (seats >= 4 and doorsNum >=4):
+    family = True 
+
+info = [modelID, bodyType, seatsNum, doorsNum, origin, weight, length, width, height]
+
+for i in range(len(info)):
+    if (info[i] == None):
+        info[i] = "Not Available"
